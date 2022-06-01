0c0bc210a8187cbe5af418d2424c643538ab94a3
Jovanni Schneider <jovanni.schneider@usach.cl>
2022-06-01 01:58:34 -0400

    corregida la matriz de adyacencia

--- main.c
+++ main.c
@@ -7,6 +7,13 @@ int main() {
     leerArchivo("pueblo1.in", &village);
     imprimirMatrizAdyacencia(village.grafo);
     recorrerLista(village.cuarteles);
+    FILE * archivo = fopen("pueblo1.in","r");
+    int cantidadAristas,a;
+    fscanf(archivo,"%d %d",&a,&cantidadAristas);
+    saltarLineasArchivo(archivo,4);
+    a = encontrarCantidadVertices(archivo,cantidadAristas);
+    printf("%d\n",a);
     liberarPueblo(&village);
+    fclose(archivo);
     return 0;
 }
--- src/functions.h
+++ src/functions.h
@@ -7,22 +7,30 @@
 
 
 void leerArchivo(char * nombreArchivo,pueblo * village);
+void saltarLineasArchivo(FILE * archivo,int cantidadLineas);
+int encontrarCantidadVertices(FILE * ,int cantLineas);
 
 void leerArchivo(char *nombreArchivo, pueblo * village){
     FILE*pf;		   //para abrir archivo
     pf = fopen(nombreArchivo,"r");
-    int cantidadVertices,i,j,k,w,cuartel;
+    int cantidadAristas,cantidadVertices,i,j,k,w,cuartel,saltoDeLineas;
     if (pf ==NULL){
         printf("Error de archivo\n");
     }else{
-        //Cantidad de nodos y aristas
-        fscanf(pf, "%d %d", &village->cantidadDeCuarteles,&cantidadVertices);
-        village->grafo = crearGrafoVacio(cantidadVertices);
+        //Cantidad de cuarteles y aristas
+        fscanf(pf, "%d %d", &village->cantidadDeCuarteles,&cantidadAristas);
         village->cuarteles = crearListaVacia();
         for (int l = 0; l < village->cantidadDeCuarteles; ++l) {
             fscanf(pf,"%d",&cuartel);
             insertarInicio(village->cuarteles,cuartel);
         }
+        printf("flag\n");
+        cantidadVertices = encontrarCantidadVertices(pf,cantidadAristas);
+        printf("%d\n",cantidadVertices);
+        village->grafo = crearGrafoVacio(cantidadVertices);
+        saltoDeLineas = village->cantidadDeCuarteles + 2;
+        printf("salto de lineas = %d\n",saltoDeLineas);
+        saltarLineasArchivo(pf,saltoDeLineas);
         //dependiendo de las lineas de archivo,
         // w es el peso (distancia) de la arista
         for(k=0;k<cantidadVertices;k++){
@@ -32,4 +40,27 @@ void leerArchivo(char *nombreArchivo, pueblo * village){
         }
         fclose(pf);
     }
+}
+
+void saltarLineasArchivo(FILE *archivo, int cantidadLineas) {
+    int i = cantidadLineas-1;
+    fseek(archivo, 0, SEEK_SET);
+    while (i > 0){
+         while(fgetc (archivo) != '\n');
+         i--;
+    }
+}
+
+int encontrarCantidadVertices(FILE *archivo,int cantLineas) {
+    int mayor = 0;
+    int aux1,aux2,aux3;
+    for (int i = 0; i < cantLineas; ++i) {
+        fscanf(archivo,"%d %d %d",&aux1,&aux2,&aux3);
+        if (mayor == 0 || aux1 > mayor)
+            mayor = aux1;
+        else if (aux2 > mayor){
+            mayor = aux2;
+        }
+    }
+    return mayor;
 }
